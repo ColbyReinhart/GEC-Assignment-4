98a096a9f968941ef499d0fb218b653426d72e18
Colby Reinhart <colbyreinhart@gmail.com>
2023-04-19 01:03:39 -0400

    Perfected car functionality

--- Assets/Scripts/PlayerVehicle.cs
+++ Assets/Scripts/PlayerVehicle.cs
@@ -3,51 +3,89 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class AxleInfo
+public class Axle
 {
     public WheelCollider leftWheel;
     public WheelCollider rightWheel;
-    public bool motor; // is this wheel attached to motor?
-    public bool steering; // does this wheel apply steer angle?
+    public bool hasMotor;
+    public bool hasSteering;
+
+    public void ApplyMotor(float torque)
+    {
+        leftWheel.motorTorque = torque;
+        rightWheel.motorTorque = torque;
+    }
+
+    public void ApplyBrake(float torque)
+    {
+        leftWheel.brakeTorque = torque;
+        rightWheel.brakeTorque = torque;
+    }
+
+    public void ApplySteering(float steerAngle)
+    {
+        Vector3 wheelEulers = leftWheel.transform.localEulerAngles;
+        wheelEulers.y = steerAngle;
+        leftWheel.transform.localEulerAngles = wheelEulers;
+        wheelEulers.y = steerAngle + 180f;
+        rightWheel.transform.localEulerAngles = wheelEulers;
+        leftWheel.steerAngle = steerAngle;
+        rightWheel.steerAngle = steerAngle;
+    }
+
+    public void ApplyHandbrake(float torque)
+    {
+        if (torque > leftWheel.brakeTorque)
+        {
+            leftWheel.brakeTorque = torque;
+        }
+        if (torque > rightWheel.brakeTorque)
+        {
+            rightWheel.brakeTorque = torque;
+        }
+
+        WheelFrictionCurve curve = leftWheel.sidewaysFriction;
+        curve.stiffness = torque > 0 ? 0.5f : 2f;
+        leftWheel.sidewaysFriction = curve;
+        rightWheel.sidewaysFriction = curve;
+    }
 }
 
 public class PlayerVehicle : MonoBehaviour
 {
-    public List<AxleInfo> axleInfos; // the information about each individual axle
-    public float maxMotorTorque; // maximum torque the motor can apply to wheel
-    public float maxSteeringAngle; // maximum steer angle the wheel can have
+    public Axle frontAxle;
+    public Axle rearAxle;
+    public float motorTorque;
+    public float brakeTorque;
+    public float handbrakeTorque;
+    public float maxSteeringAngle;
 
-    // finds the corresponding visual wheel
-    // correctly applies the transform
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Finds the corresponding visual wheel and correctly applies the transform
     public void ApplyLocalPositionToVisuals(WheelCollider collider)
     {
-        Transform visualWheel = collider.transform;
+        Transform wheelObject = collider.transform;
 
         Vector3 position;
         Quaternion rotation;
         collider.GetWorldPose(out position, out rotation);
 
-        visualWheel.transform.position = position;
-        visualWheel.transform.rotation = rotation;
+        wheelObject.transform.position = position;
+        wheelObject.transform.rotation = rotation;
     }
 
     public void FixedUpdate()
     {
-        float motor = maxMotorTorque * Input.GetAxis("Vertical");
-        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
-
-        foreach (AxleInfo axleInfo in axleInfos)
-        {
-            if (axleInfo.steering)
-            {
-                axleInfo.leftWheel.steerAngle = steering;
-                axleInfo.rightWheel.steerAngle = steering;
-            }
-            if (axleInfo.motor)
-            {
-                axleInfo.leftWheel.motorTorque = motor;
-                axleInfo.rightWheel.motorTorque = motor;
-            }
-        }
+        rearAxle.ApplyMotor(Input.GetAxis("Gas") * motorTorque);
+        frontAxle.ApplyBrake(Input.GetAxis("Brake") * brakeTorque);
+        rearAxle.ApplyBrake(Input.GetAxis("Brake") * brakeTorque);
+        frontAxle.ApplySteering(Input.GetAxis("Steering") * maxSteeringAngle);
+        rearAxle.ApplyHandbrake(Input.GetAxis("Handbrake") * handbrakeTorque);
     }
 }

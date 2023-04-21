a8959337a284a45e4443e6f59edca36cb2af9f09
Colby Reinhart <colbyreinhart@gmail.com>
2023-04-21 17:35:52 -0400

    Created lap timer

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -5,10 +6,44 @@ using UnityEngine;
 
 public class UIController : MonoBehaviour
 {
-    public TMP_Text lapTimer;
+    public TMP_Text currentLapText;
+    public TMP_Text bestLapText;
+
+    public string timerFormat = "mm':'ss'.'f";
+
+    private bool raceStarted = false;
+    private float currentLapStamp = 0f;
+    private float bestLapTime = 0f;
 
     public void Lap()
     {
+        // Start counting
+        if (!raceStarted)
+        {
+            raceStarted = true;
+            currentLapStamp = Time.time;
+            return;
+        }
 
+        // Calculate lap time
+        float lapTime = Time.time - currentLapStamp;
+        currentLapStamp = Time.time;
+
+        // Was this the best lap?
+        if (lapTime < bestLapTime || bestLapTime == 0f)
+        {
+            bestLapTime = lapTime;
+            bestLapText.text =
+                "Best: " + TimeSpan.FromSeconds(lapTime).ToString(timerFormat);
+        }
+    }
+
+    private void Update()
+    {
+        if (!raceStarted) { return; }
+
+        float timeSinceLapStart = Time.time - currentLapStamp;
+        currentLapText.text =
+            "Lap: " + TimeSpan.FromSeconds(timeSinceLapStart).ToString(timerFormat);
     }
 }
